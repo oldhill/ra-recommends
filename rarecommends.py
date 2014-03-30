cd5678640e17cca0517c14f7d172b2ca1f10a560
oldhill <oldhillcode@gmail.com>
2014-03-30 01:06:43 +0000

    Fix Resident Advisor regex to match new page design

--- rarecommends.py
+++ rarecommends.py
@@ -11,13 +11,11 @@ import re
 
 
 def getResidentData():
-  # grab all the data...
   web_page = urllib2.urlopen('http://www.residentadvisor.net/reviews.aspx?format=recommend')
   all_the_html = web_page.read()
-  demarcator = 'pb2' # surrounds records of recommended stuff
-  relevant_block = re.findall(demarcator+'(.*?)'+demarcator, all_the_html, re.DOTALL)[0]
+  relevant_block = re.findall('reviewArchive'+'(.*?)'+'</article>', all_the_html, re.DOTALL)[0]
   # zeroing in...
-  relevant_string = re.findall('class=music>'+'(.*?)'+'</a', relevant_block, re.DOTALL)[0]
+  relevant_string = re.findall('<h1>'+'(.*?)'+'</h1>', relevant_block, re.DOTALL)[0]
   return relevant_string
 
 
